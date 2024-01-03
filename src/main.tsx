676acb4edf2938376852d0e9ee9f27e5baf2e1da
skdding3 <skdding3@gmail.com>
2024-01-03 20:12:09 +0900

    [fix] mock worker module fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { worker } from "./mock/browser";
+import { worker } from "./src/mock/browser.js";
 
 worker.start();
 
